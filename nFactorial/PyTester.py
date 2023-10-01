340d7f179eb43ac7135705179825b70cd60ddb9b
GoetzDeBouville <31970713+GoetzDeBouville@users.noreply.github.com>
2023-10-01 23:55:45 +0300

    py code corrected

--- nFactorial/PyTester.py
+++ nFactorial/PyTester.py
@@ -1,21 +1,16 @@
 import random
 import time
+import itertools
 
 def arrayBuild(num):
     return [random.randint(0, 999) for _ in range(num)]
 
-def rearangeArr(arr, start):
-    for i in range(start, len(arr)):
-        arr[start], arr[i] = arr[i], arr[start]
-        rearangeArr(arr, start + 1)
-        arr[start], arr[i] = arr[i], arr[start]
-
 if __name__ == "__main__":
     array = arrayBuild(11)
 
-start_time = time.time()
-rearangeArr(array, 0)
-end_time = time.time()
+    start_time = time.time()
+    all_permutations = list(itertools.permutations(array))
+    end_time = time.time()
 
-elapsed_time = end_time - start_time
-print(f"{elapsed_time:.5f} sec")
+    elapsed_time = end_time - start_time
+    print(f"{elapsed_time:.5f} sec")
